4c8e4bda659336b778b03b3aba6a2b1005662ff1
kuso-megane <orikazu.0824@i.softbank.jp>
2021-11-12 12:08:05 +0900

    routing 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'linebot/main'
     root 'welcome#index'
     get 'welcome/index' => 'welcome#index'
-    post 'api/linebot' => 'linebot/main'
+    post '/callback' => 'webhook#callback'
 end
